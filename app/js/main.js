7bc203c46904b503c1d8b91ff71c7823f374cd57
Aloxan12 <kvakvaaak@mail.ru>
2022-04-12 00:01:08 +0300

    header scss

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,9 @@ $(function () {
 
     $('.header__slider').slick({
         infinite: true,
-        fade: true
+        fade: true,
+        prevArrow: `<img class="slider-arrows slider-arrows-left" src="img/arrow-left.svg" alt="">`,
+        nextArrow: `<img class="slider-arrows slider-arrows-right" src="img/arrow-right.svg" alt="">`
     });
 
 
