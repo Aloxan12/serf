c6d8e733753c67b385b93bc2bf0525983eec3bc7
Aloxan12 <kvakvaaak@mail.ru>
2022-04-12 00:16:13 +0300

    header scss

--- app/js/main.js
+++ app/js/main.js
@@ -5,13 +5,13 @@ $(function () {
         fade: true,
         prevArrow: `<img class="slider-arrows slider-arrows-left" src="img/arrow-left.svg" alt="">`,
         nextArrow: `<img class="slider-arrows slider-arrows-right" src="img/arrow-right.svg" alt="">`,
-        asNavFor: '.slider-dots',
+        asNavFor: `.slider-dots`,
     });
 
     $('.slider-dots').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
-        asNavFor: '.header__slider',
+        asNavFor: `.header__slider`,
     });
 
 });
